651837b84af5b8292fb222b6f74157b3733935f4
Jaime A. Chaves <politologoamigo@gmail.comgit config --list--help>
2020-11-09 19:47:04 -0500

    Single Menu Component Improvement

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-  function RenderMenuItem({dish, onClick}) {
-    return(
-      <Card> 
-        <Link to={`/menu/${dish.id}`} ></Link>
-      <CardImg width="100%" object src={dish.image} alt={dish.name} />
-        <CardImgOverlay>
-          <CardTitle>{dish.name}</CardTitle>
-        </CardImgOverlay>
-        </Link>
+function RenderMenuItem ({dish, onClick}) {
+  return (
+      <Card>
+          <Link to={`/menu/${dish.id}`} >
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardImgOverlay>
+                  <CardTitle>{dish.name}</CardTitle>
+              </CardImgOverlay>
+          </Link>
       </Card>
-    );
-  }
+  );
+}
 
   const Menu = (props) => {
 
@@ -40,7 +40,7 @@ import { Link } from 'react-router-dom';
     <div className="row">
         {menu}
     </div>
-</div>
+</div>  
   );
   }
 
